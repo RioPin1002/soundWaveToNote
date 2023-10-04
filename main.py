8c894292d623205eb2104ff9d0a257929e8c27c1
RioPin1002 <RioPinE235@icloud.com>
2023-10-04 22:13:23 +0900

    version 1.2

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import numpy as np
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 from scipy import signal
+from scipy.signal import find_peaks
 
 device_list = sd.query_devices()
 print(device_list)
 
 sd.default.device = [1, 6] # type: ignore # Input, Outputデバイス指定
+peak_freqs = []  # ピークの周波数を格納
+peak_amplitudes = []  # ピークの振幅を格納
 
 def callback(indata, frames, time, status):
     # indata.shape=(n_samples, n_channels)
@@ -22,27 +25,36 @@ def callback(indata, frames, time, status):
 def update_plot(frame):
     """This is called by matplotlib for each plot update.
     """
-    global plotdata, window
-    x = plotdata[-N:] * window
+    global plotdata, window, peak_freqs, peak_amplitudes
+    x = plotdata[-N:]
     F = np.fft.fft(x) # フーリエ変換
     F = F / (N / 2) # フーリエ変換の結果を正規化
-    F = F * (N / sum(window)) # 窓関数による補正
+    F = F * (N) # 窓関数による補正
     Amp = np.abs(F) # 振幅スペクトル
     line.set_ydata(Amp[:N // 2])
+
+     # ピーク検出
+    peaks, _ = find_peaks(Amp[:N // 2], height=7)  # 閾値を調整可能
+    peak_freqs = freq[:N // 2][peaks]  # 周波数の配列からピーク周波数を取得
+    peak_amplitudes = Amp[:N // 2][peaks]  # 振幅の配列からピーク振幅を取得
+
+    print(peak_freqs)
 
     return line,
 
 downsample = 1  # FFTするのでダウンサンプリングはしない
 length = int(1000 * 48000 / (1000 * downsample))
 plotdata = np.zeros((length))
-N =2048            # FFT用のサンプル数
+N =4096            # FFT用のサンプル数
 fs = 48000            # 音声データのサンプリング周波数
 window = signal.hann(N) # 窓関数 # type: ignore
 freq = np.fft.fftfreq(N, d=1 / fs) # 周波数スケール
 
 fig, ax = plt.subplots()
 line, = ax.plot(freq[:N // 2], np.zeros(N // 2))
-ax.set_ylim([0, 0.5]) # type: ignore
+# ピークをプロットに追加
+ax.plot(peak_freqs, peak_amplitudes, 'ro', markersize=5)  # ピークを赤い点でプロット
+ax.set_ylim([0, 100]) # type: ignore
 ax.set_xlim([0, 1000]) # type: ignore
 ax.set_xlabel('Frequency [Hz]')
 ax.set_ylabel('Amplitude spectrum')
--- microphone.py
+++ microphone.py
@@ -3,17 +3,17 @@ import sounddevice as sd
 # 利用可能なオーディオデバイスのリストを取得
 device_list = sd.query_devices()
 
-sd.default.device = [1,6]
+sd.default.device = [1,6] # type: ignore
 
 # マイクのデバイス情報を特定
 microphone_device = None
 for device_info in device_list:
-    if "Microphone" in device_info["name"]:
+    if "Microphone" in device_info["name"]: # type: ignore
         microphone_device = device_info
         break
 
 # マイクのサンプリングレートを表示
 if microphone_device:
-    print(f"マイクのサンプリングレート: {microphone_device['default_samplerate']} Hz")
+    print(f"マイクのサンプリングレート: {microphone_device['default_samplerate']} Hz") # type: ignore
 else:
     print("マイクが見つかりませんでした。")
